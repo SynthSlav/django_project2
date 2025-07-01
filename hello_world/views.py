b0b52f30d569d8b3880058be402e340bc74af72c
SynthSlav <ljupcebozinovski@gmail.com>
2025-07-01 23:00:57 +0200

    deploy to Heroku to practice deployments

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello_world index.")
+    if request.method == 'POST':
+        return HttpResponse("Hello, World! This is the index page of our Django application.")
+    else:
+        return HttpResponse(request.method)
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^u3!5t#szdo^%8&w0pe8rjaz#y@me1em-gm#h1q1ji&oab45ga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuatt.com', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',  # Add your app here
+    'about',  # Add your app here
 ]
 
 MIDDLEWARE = [
